2ce95a5101cc2002fdc9de88c09e019663e8df7c
xiayongqing <xiayongqingq@163.com>
2019-01-25 16:17:26 +0800

    创建数据库脚本文件xiaofeiniu.sql

--- xiaofeiniu.sql
+++ xiaofeiniu.sql
@@ -7,15 +7,18 @@ USE xfn;
 #管理员信息表：xfn_admin
 CREATE TABLE xfn_admin(
   aid INT PRIMARY KEY AUTO_INCREMENT, #管理员编号
-	aname VARCHAR(32) UNIQUE,           #管理员编号
+  aname VARCHAR(32) UNIQUE,           #管理员编号
   apwd  VARCHAR(64)                   #管理员密码
-)
+);
+INSERT INTO xfn_admin VALUES
+(NULL,'admin',PASSWORD('123456')),
+(NULL,'boss',PASSWORD('666666'));
 
 
 #项目全局设置：xfn_settings
 CREATE TABLE xfn_settings(
   sid INT PRIMARY KEY AUTO_INCREMENT,
-	appname VARCHAR(32),    #应用/店家名称
+  appname VARCHAR(32),    #应用/店家名称
   apiURL  VARCHAR(64),    #数据api子系统地址
   adminUrl VARCHAR(64),   #管理后台子系统地址
   appUrl   VARCHAR(64),   #顾客app子系统地址
@@ -34,10 +37,10 @@ CREATE TABLE xfn_table(
 #桌台预订信息表：xfn_reservation
 CREATE TABLE xfn_reservation{
    rid INT PRIMARY KEY AUTO_INCREMENT,
-	 contactName VARCHAR(64), #联系人姓名
+   contactName VARCHAR(64), #联系人姓名
    phone  VARCHAR(16),      #联系电话
    contactTime BIGINT,      #联系时间
-	 dinnerTime BIGINT        #预约的用餐时间
+   dinnerTime BIGINT        #预约的用餐时间
 }
 
 #菜品分类表：xfn_category
@@ -50,10 +53,10 @@ CREATE TABLE xfn_category{
 CREATE TABLE xfn_dish{
   did INT PRIMARY KEY AUTO_INCREMENT,
   dname VACHAR(32),
-	imgUrl VARCHAR(128),
-	price DECIMAL(6,2),
-	details VARCHAR(128),
-  categoryId INT
+  imgUrl VARCHAR(128),
+  price DECIMAL(6,2),
+  details VARCHAR(128),
+  categoryId INT FOREIGN KRY
 }
 
 #订单表：xfn_order
@@ -62,13 +65,14 @@ CREATE TABLE xfn_order{
   startTime  BIGINT,
   endTime BIGINT,
   customerCount INT,
-  tableId INT
+  tableId INT FOREIGN KRY
 }
 
 #订单表  xfn_order_detail
 CREATE TABLE xfn_order_detail{
   did INT PRIMARY KEY AUTO_INCREMENT,
-  dishId INT,
+  dishId INT FOREIGN KRY,
   dishCount INT,
-  customerName VARCHAR(64)
+  customerName VARCHAR(64),
+  orderId  INT FOREIGN KRY
 }
